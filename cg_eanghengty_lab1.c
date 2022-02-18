0c695018d7f603cf2f41c30d29b212219448db24
eanghengty <hengty174@gmail.com>
2022-02-19 06:53:54 +0700

    lab2

--- cg_eanghengty_lab1.c
+++ cg_eanghengty_lab1.c
@@ -4,14 +4,14 @@
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-    glColor4f(1.0f, 0.0f, 0.0f, 0.0f);//Green
+    glColor4f(1.0f, 0.0f, 0.0f, 0.0f);//Red
     glVertex2f(0.2,0.2);
     glVertex2f(0.4,0.6);
     glVertex2f(0.6,0.2);
     
     glEnd();
     glBegin(GL_POLYGON);
-    glColor3f(0.0f, 0.0f, 1.0f);//Green
+    glColor4f(1.0f, 0.0f, 0.0f,0.0f);//blue
     for(int i=0;i <360;i++){
       double angle= i * 3.14/180;
       glVertex2f((1.0 + 0.3 * sin(angle)),(0.4 + 0.3 * cos(angle)));
@@ -28,8 +28,7 @@ int main(int argc , char** argv){
     glClearColor(1.0,1.0,1.0,0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0.0,2.0,0.0,2.0);
-    glutDisplayFunc(display);
+    gluOrtho2D(0.0,2.0,0.0,2.0);//how far or how near the object to the screen
     glutMainLoop();
     return 0;
 }
---
+++ lab2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <GL/glut.h>
+int main(){
+    //this is the main point to start
+}
