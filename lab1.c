6e080923b445b30c0a318792b02825483a5dc3ea
eanghengty <hengty174@gmail.com>
2022-02-19 16:09:23 +0700

    lab2

---
+++ lab1.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<GL/glut.h>
+void display(){
+   
+    glClear(GL_COLOR_BUFFER_BIT);
+   
+    glBegin(GL_LINES);
+    
+    glVertex2f(0.2,0.1);
+    glVertex2f(0.5,0.1);
+    glEnd();
+    glFlush();
+}
+int main(int argc , char** argv){
+    glutInit(&argc , argv);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+     glutCreateWindow("lab1");
+    
+    // glClearColor (1.0, 1.0, 1.0, 0.0);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0,3.0,0.0,3.0);
+   
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
+
+// #include <GL/glut.h>
+// #include <stdio.h>
+// void display(void)
+// {
+//  // clear background of current window
+//  glClear(GL_COLOR_BUFFER_BIT);
+//  // draw a filled polygon
+//  glBegin(GL_POLYGON);
+
+//  glVertex2i(75,85);
+//  glVertex2i(20,40);
+//  glVertex2i(130,40);
+//  glEnd();
+//  glFlush ();
+// }
+// void init(void)
+// {
+
+
+//  glClearColor (1.0, 1.0, 1.0, 0.0);
+ 
+//  glMatrixMode(GL_PROJECTION);
+
+//  glLoadIdentity();
+//  gluOrtho2D(0, 150.0, 0, 150.0);
+// }
+// int main(int argc, char** argv)
+// {
+//  glutInit(&argc, argv);
+
+//  glutInitWindowSize (500,500);
+//  glutInitWindowPosition (100, 100);
+//  glutCreateWindow ("Simple Drawing");
+//  init();
+//  glutDisplayFunc(display);
+//  glutMainLoop();
+// return 0;
+// }
---
+++ lab2.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<GL/glut.h>
+#define maxSize 480.0
+#define minSize 20.0
+void display(){
+    float half = (maxSize-20)/(minSize*2.0);
+    float mid = (maxSize/2.0)+ 10.0;
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINES);
+    glVertex2f(minSize,mid);
+    glVertex2f(maxSize,mid);
+    glVertex2f(mid,maxSize);
+    glVertex2f(mid,minSize);
+    for(float i= half ; i > 1.0  ; i--){
+        glVertex2f(minSize*i,255.0);
+        glVertex2f(minSize*i,245.0);
+    }
+    for(float i=1.0 ; i < half  ; i++){
+        glVertex2f(mid + (20.0*i),255.0);
+        glVertex2f(mid + (20.0*i),245.0);
+    }
+    for(float i=half; i > 1.0  ; i--){
+        glVertex2f(245.0,20.0*i);
+        glVertex2f(255.0,20.0*i);
+    }
+    for(float i=1.0; i < half  ; i++){
+        glVertex2f(245.0,mid+(20.0*i));
+        glVertex2f(255.0,mid+(20.0*i));
+    }
+    //arrow y
+    glVertex2f(240.0,470.0);
+    glVertex2f(mid,maxSize);
+    glVertex2f(mid,maxSize);
+    glVertex2f(260.0,470.0);
+    //arrow x
+    glVertex2f(470.0,240.0);
+    glVertex2f(maxSize,mid);
+    glVertex2f(maxSize,mid);
+    glVertex2f(470.0,260.0);
+    glEnd();
+    glFlush();
+
+}
+void view(){
+   
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluOrtho2D(0.0,500.0,0.0,500.0);
+}
+int main(int argc , char** argv){
+    glutInit(&argc,argv);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("lab2");
+    view();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
